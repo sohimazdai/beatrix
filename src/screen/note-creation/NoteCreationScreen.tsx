3d8fa9dce629c67cab39a5553645d0427e988e1c
Andrey Pankratov <i@doq.su>
2019-08-14 00:53:02 +0300

    n8: add slider and styles to inputBlock

--- src/screen/note-creation/NoteCreationScreen.tsx
+++ src/screen/note-creation/NoteCreationScreen.tsx
@@ -4,7 +4,10 @@ import {
     Text,
     TextInput,
     StyleSheet,
-    TouchableOpacity
+    TouchableOpacity,
+    Slider,
+    SliderComponent,
+    SliderBase
 } from 'react-native';
 import { connect } from 'react-redux';
 import { Dispatch, Action } from 'redux';
@@ -15,6 +18,14 @@ import { NavigationScreenProp, NavigationParams } from 'react-navigation';
 import { NavigationState } from 'react-navigation';
 import { ThemeColor } from '../../constant/ThemeColor';
 import { Hat } from '../../component/hat/Hat';
+import * as lodash from "lodash"
+
+const sliderOptions = {
+    maximumValue: 12,
+    minimumValue: 0,
+    step: 0.1,
+    minimumTrackTintColor: '#999999'
+}
 
 interface NoteCreationScreenProps {
     dispatch: (action: Action) => void,
@@ -39,14 +50,7 @@ class NoteCreationScreen extends React.Component<NoteCreationScreenProps, NoteCr
         return (
             <View style={styles.noteCreationView}>
                 {this.renderInputs()}
-                <TouchableOpacity
-                    style={styles.saveButton}
-                    onPress={this.createNote}
-                >
-                    <Text style={styles.saveButtonText}>
-                        Записать
-                    </Text>
-                </TouchableOpacity>
+                {this.renderSaveButton()}
                 <Hat
                     title={'Новая запись'}
                     onBackPress={() => this.props.navigation.navigate('NoteList')}
@@ -67,25 +71,84 @@ class NoteCreationScreen extends React.Component<NoteCreationScreenProps, NoteCr
     }
 
     renderInputs() {
+        const { glucoseInput, breadUnitsInput, insulinInput } = this.state
+        const glucoseSliderValue = glucoseInput ?
+            Math.floor(parseFloat(glucoseInput)) : 0;
+        const breadUnitsSliderValue = breadUnitsInput ?
+            parseFloat(breadUnitsInput) : 0;
+        const insulinSliderValue = insulinInput ?
+            parseFloat(insulinInput) : 0;
+
         return (
-            <View>
-                <NoteInput
-                    placeholder={'Глюкоза'}
-                    value={this.state.glucoseInput}
-                    onChangeText={(text) => this.setState({ glucoseInput: text })}
-                />
-                <NoteInput
-                    placeholder={'ХЕ'}
-                    value={this.state.breadUnitsInput}
-                    onChangeText={(text) => this.setState({ breadUnitsInput: text })}
-                />
-                <NoteInput
-                    placeholder={'Инсулин'}
-                    value={this.state.insulinInput}
-                    onChangeText={(text) => this.setState({ insulinInput: text })}
-                />
+            <View style={styles.inputBlock}>
+                <View style={styles.input}>
+                    <NoteInput
+                        placeholder={'Глюкоза'}
+                        value={this.state.glucoseInput}
+                        onChangeText={(text) => this.setState({ glucoseInput: text })}
+                    />
+                </View>
+                <View style={styles.slider}>
+                    <Slider
+                        value={glucoseSliderValue}
+                        onValueChange={lodash.debounce((value) =>
+                            this.setState({
+                                glucoseInput: (Math.floor(value * 10) / 10).toString()
+                            }), 50
+                        )}
+                        {...sliderOptions}
+                    />
+                </View>
+                <View style={styles.input}>
+                    <NoteInput
+                        placeholder={'ХЕ'}
+                        value={this.state.breadUnitsInput}
+                        onChangeText={(text) => this.setState({ breadUnitsInput: text })}
+                    />
+                </View>
+                <View style={styles.slider}>
+                    <Slider
+                        value={breadUnitsSliderValue}
+                        onValueChange={lodash.debounce((value) =>
+                            this.setState({
+                                breadUnitsInput: (Math.floor(value * 10) / 10).toString()
+                            }), 50
+                        )}
+                        {...sliderOptions}
+                    />
+                </View>
+                <View style={styles.input}>
+                    <NoteInput
+                        placeholder={'Инсулин'}
+                        value={this.state.insulinInput}
+                        onChangeText={(text) => this.setState({ insulinInput: text })}
+                    />
+                </View>
+                <View style={styles.slider}>
+                    <Slider
+                        value={insulinSliderValue}
+                        onValueChange={lodash.debounce((value) =>
+                            this.setState({
+                                insulinInput: (Math.floor(value * 10) / 10).toString()
+                            }), 50
+                        )}
+                        {...sliderOptions}
+                    />
+                </View>
             </View>
         )
+
+
+    }
+
+    renderSaveButton() {
+        return <View style={styles.saveButton}>
+            <TouchableOpacity onPress={this.createNote}>
+                <Text style={styles.saveButtonText}>
+                    Записать
+                </Text>
+            </TouchableOpacity>
+        </View>
     }
 }
 
@@ -96,16 +159,49 @@ const styles = StyleSheet.create({
         minHeight: '100%',
 
         alignItems: 'center',
+        backgroundColor: "#4B5860",
+    },
+    inputBlock: {
+        width: '100%',
+
+        marginTop: 65,
+        paddingBottom: 15,
+
+        elevation: 2,
+
+        shadowOffset: { width: 10, height: 10 },
+        shadowColor: '#000',
+        shadowRadius: 5,
+        shadowOpacity: 1,
+
+        borderRadius: 25,
+
+        alignItems: 'center',
+        backgroundColor: "#E9E6DA",
+    },
+    input: {
+        padding: 15,
+    },
+    slider: {
+        width: 280,
+        paddingBottom: 10,
     },
     saveButton: {
         width: 150,
         height: 50,
 
-        borderRadius: 15,
+        margin: 20,
 
-        alignItems: 'center',
+        elevation: 2,
+        shadowOffset: { width: 10, height: 10 },
+        shadowColor: '#000',
+        shadowRadius: 5,
+        shadowOpacity: 1,
+
+        borderRadius: 15,
         justifyContent: 'center',
 
+        alignItems: 'center',
         backgroundColor: ThemeColor.LIGHT_RED,
     },
     saveButtonText: {
