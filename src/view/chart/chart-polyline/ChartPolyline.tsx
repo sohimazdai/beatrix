027fc727465ee3799f8f8a45446f54403981fd78
Andrey Pankratov <i@doq.su>
2019-09-02 23:56:20 +0300

    n26: add gradient to polyline fill

--- src/view/chart/chart-polyline/ChartPolyline.tsx
+++ src/view/chart/chart-polyline/ChartPolyline.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Circle, Polyline } from 'react-native-svg';
+import { Circle, Polyline, LinearGradient, Stop } from 'react-native-svg';
 import { ThemeColor } from '../../../constant/ThemeColor';
 import { IChartDot } from '../../../model/IChart';
 
@@ -12,10 +12,17 @@ export function ChartPolyline(props: ChartPolylineProps) {
     Object.keys(props.dots).sort((a, b) => parseInt(a) - parseInt(b)).map(time => {
         points += props.dots[time].x + ',' + props.dots[time].y + ' '
     })
-    return <Polyline
-        points={points}
-        stroke="tomato"
-        strokeWidth={1}
-        strokeLinecap='round'
-    />
+    return <>
+        <LinearGradient id="grad" x1="0" y1="0" x2="0" y2="100%">
+            <Stop offset="0%" stopColor="#7C89FF" stopOpacity="1" />
+            <Stop offset="100%" stopColor="#7C3869" stopOpacity="1" />
+        </LinearGradient>
+        <Polyline
+            points={points}
+            stroke="rgba(255, 255, 255, 0.64)"
+            strokeWidth={2}
+            strokeLinecap='round'
+            fill="url(#grad)"
+        />
+    </>
 }
