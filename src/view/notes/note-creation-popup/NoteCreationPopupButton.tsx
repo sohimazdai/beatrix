81bf5b333c76ccb2c7654b6562e78a6ffcb9bd71
Andrey Pankratov <i@doq.su>
2020-02-03 00:16:43 +0300

    n27: fix note creating button layout

--- src/view/notes/note-creation-popup/NoteCreationPopupButton.tsx
+++ src/view/notes/note-creation-popup/NoteCreationPopupButton.tsx
@@ -11,39 +11,48 @@ interface Props {
 }
 
 const NoteCreationPopupButton = (props: Props) => (
-    <TouchableOpacity onPress={props.openPopup}>
-        <View style={styles.addNoteButton}>
+    <View style={styles.addNoteButton}>
+        <TouchableOpacity
+            onPress={props.openPopup}
+            style={styles.addNoteButtonTouchable}
+        >
             <Text style={styles.addNoteButtonText}>
                 Записать
                 </Text>
             <AddNoteIcon />
-        </View>
-    </TouchableOpacity>
+        </TouchableOpacity>
+    </View>
 )
 
 export const NoteCreationPopupButtonConnect = connect(
     () => ({}),
     (dispatch) => ({
-        openPopup: () => { dispatch(createChangeInteractive({ 
-            creatingNoteMode: true,
-            editingNoteId: ""
-        })) }
+        openPopup: () => {
+            dispatch(createChangeInteractive({
+                creatingNoteMode: true,
+                editingNoteId: ""
+            }))
+        }
     })
 )(NoteCreationPopupButton)
 
 const styles = StyleSheet.create({
     addNoteButton: {
-        display: 'flex',
         padding: 5,
         margin: 5,
         paddingLeft: 10,
         paddingRight: 10,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
+
         backgroundColor: 'rgba(250,250,250, 1)',
         borderRadius: 30,
         ...shadowOptions
+    },
+    addNoteButtonTouchable: {
+        display: 'flex',
+
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     addNoteButtonText: {
         fontSize: 18,
