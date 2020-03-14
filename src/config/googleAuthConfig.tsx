7754eef537b6625e57b96b564b8726cc81c0ae6a
Andrey Pankratov <i@doq.su>
2020-03-14 20:04:14 +0300

    n29: replace firebase and google configs

---
+++ src/config/googleAuthConfig.tsx
@@ -0,0 +1,8 @@
+export const googleAuthConfig = {
+    expoClientId: '561561506983-7v3977hbb5se5rpk4arqpg2m6jnvs3j7.apps.googleusercontent.com',
+    expoClientSecret: 'VdxFV6i_0rCBhoQpWg5hAsPM',
+    androidClientId: '561561506983-642b95m6rd3lqjo31pk7n1b8v5p3trdm.apps.googleusercontent.com',
+    iosClientId: '561561506983-bnpbobcn0mtbap7g27p91dlpopg11btr.apps.googleusercontent.com',
+    androidStandaloneAppClientId: '561561506983-rr75qq0eu7cmd0qmubb4js0tmeugeplt.apps.googleusercontent.com',
+    iosStandaloneAppClientId: '561561506983-m53hgcq6e9bmirbtims5tmq1tlv5tms0.apps.googleusercontent.com',
+}
