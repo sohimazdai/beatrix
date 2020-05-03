65fd965bc3800c4b88ae1374df20ea244b709054
Andrey Pankratov <sohimazdai@gmail.com>
2020-05-03 19:12:00 +0300

    n33: fix dev stub bug

--- src/component/dev-stub/DevStub.tsx
+++ src/component/dev-stub/DevStub.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 export function DevStub() {
-  return <View style={{ position: 'absolute'}}>
+  if (!__DEV__) return null;
+
+  return <View style={{ position: 'absolute' }}>
     <Text style={{ color: 'red', fontWeight: "bold" }}>
       DEVELOPMENT MODE
     </Text>
